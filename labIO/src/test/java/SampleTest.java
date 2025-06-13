1fee50f1ab9279884dca37d50f5a82abc6e7f585
ertup1 <macieksz2004@wp.pl>
2025-06-13 19:22:37 +0200

    Add files via upload

---
+++ labIO/src/test/java/SampleTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        int tmp = 4;
+        int result = 2 + 2;
+        assert result == tmp;
+    }
+
+    @Test
+    public void sampleTest2() {
+        int tmp = 7;
+        int result = 9 - 2;
+        assert result == tmp;
+    }
+
+}
