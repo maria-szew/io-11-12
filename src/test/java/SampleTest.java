097596639a8706750f21f083f05eaf56064fd8e8
maria-szew <maria.szewczyk@student.wat.edu.pl>
2025-06-16 20:42:20 +0200

    Update SampleTest.java

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class SampleTest {
 
     @Test
-    public void sampleTest() {
+    public void sampleTest( {
         int tmp = 4;
         int result = 2 + 2;
         assert result == tmp;
